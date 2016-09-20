c1b8cb3aec17ecf03ffcf24cb68f705464252f71
czkct <2504621508@qq.com>
2016-09-20 10:17:25 +0800

    demon--反射--利用反射机制来调用多种方法

---
+++ methods/src/methods/MethodTableTest.java
@@ -0,0 +1,54 @@
+package methods;
+
+import java.lang.reflect.Method;
+
+/**
+ * @Title : MethodPointerTest.java
+ * @author: Pro.DU
+ * @date  : 2016年9月20日上午7:45:37
+ * @bolg  : www.produ.pw 
+ * @email : 2504621508@qq.com 
+ * @Description: This program shows how to invoke methods through reflection.//利用反射调用方法的demon
+ */
+public class MethodTableTest {
+	public static void main(String[] args) throws Exception
+	{
+		//get method pointers to the square and sqrt methods
+		Method square = MethodTableTest.class.getMethod("square",double.class);
+		Method sqrt = Math.class.getMethod("sqrt", double.class);
+		
+		//print tables of x- and y-values
+		
+		printTable(1, 10, 10, square);
+		printTable(1, 10, 10, sqrt);
+	}
+	/**
+	 * Returns the square of a number
+	 * @param x a number
+	 * @return x squared
+	 */
+	public static double square(double x) 
+	{
+		return x*x;
+	}
+	
+	public static void printTable(double from, double to, int n, Method f)
+	{
+		//print out the method as table header
+		
+		System.out.println(f);
+		double dx = (to - from) / (n-1);
+		for(double x = from; x <= to; x += dx)
+		{
+			try
+			{
+				double y = (Double) f.invoke(null, x);
+				System.out.printf("%10.4f | %10.4f%n", x, y);
+			}
+			catch(Exception e)
+			{
+				e.printStackTrace();
+			}
+		}
+	}
+}
